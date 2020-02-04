e783f4fe5cd676c360bf6058668c0a501a257417
fffengb <644683848@qq.com>
2020-02-04 15:03:38 +0800

    完善了ConfBaseRequire表的CRUD。 更正了ConfBaseRequire的url路径。

--- config/src/main/java/org/qm/config/controller/ConfBaseController.java
+++ config/src/main/java/org/qm/config/controller/ConfBaseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/cfg")
+@RequestMapping("/cfg/base")
 @Validated
 public class ConfBaseController {
     private ConfBaseService confBaseService;
@@ -27,5 +27,28 @@ public class ConfBaseController {
         return new Result(ResultCode.SUCCESS, confBaseService.add(confBase));
     }
 
+    //删除一个基本设置
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable String id) {
+        return new Result(ResultCode.SUCCESS, confBaseService.deleteById(id));
+    }
+
+    //修改一个基本设置
+    @RequestMapping(method = RequestMethod.PATCH)
+    public Result update(@RequestBody ConfBaseInquire confBase) {
+        return new Result(ResultCode.SUCCESS, confBaseService.update(confBase));
+    }
+
+    //查找所有基本设置
+    @RequestMapping(method = RequestMethod.GET)
+    public Result findAll() {
+        return new Result(ResultCode.SUCCESS, confBaseService.findAll());
+    }
+
+    //根据ID查找一共设置
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Result findById(@PathVariable String id) {
+        return new Result(ResultCode.SUCCESS, confBaseService.findById(id));
+    }
 
 }
